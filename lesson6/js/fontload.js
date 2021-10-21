141239ef773c500a545ff4854e377ce09918b711
leyakino <neoaquino@byui.edu>
2021-10-21 17:31:39 +0800

    upload lesson 6

---
+++ lesson6/js/fontload.js
@@ -0,0 +1,13 @@
+/*WebFont.load({
+  google: {
+    families: [
+      'Poppins: 400, 500, 600, 700, 800'
+    ]
+  }
+});*/
+
+WebFontConfig = {
+  google: {
+    families: ['Poppins: 400, 500, 600, 700, 800']
+  }
+};
